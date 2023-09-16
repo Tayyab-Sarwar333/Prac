eaa1ac0523c69964964ddfcdc653cfcf3fe29ca1
Muhammad <222muhammadtayyab@gmail.com>
2023-09-16 00:13:00 -0700

    Conditional opreator

--- app1.js
+++ app1.js
@@ -3,3 +3,80 @@
 
 // console.log(message);
 // yahoo()
+
+
+// let day; 
+//  switch(Date.getDays())
+//  case 0:
+
+
+
+//  let x = prompt("Enter Your Value");
+// switch (x) {
+//   case x=0:
+//    console.log("Sunday");
+//     break;
+//   case x=1:
+//    console.log("Mondy");
+//     break;
+//   case x=2:
+//     console.log("Tuesday");
+//     break;
+//     case x=3:
+//    console.log("Wednesday");
+//     break;
+//     case x=4:
+//    console.log("Thrusday");
+//     break;
+//     case x=5:
+//    console.log("Friday");
+//     break;
+//     case x=6:
+//    console.log("Saturday");
+//     break;
+//   default:
+//     console.log("No value found");
+// }
+
+
+
+// program for a simple calculator
+// let result;
+
+// // take the operator input
+// const operator = prompt('Enter operator ( either +, -, * or /; %; ** ): ');
+
+// // take the operand input
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// switch(operator) {
+//     case '+':
+//         result = number1 + number2;
+//         console.log(`${number1} + ${number2} = ${result}`);
+//         break;
+//     case '-':
+//         result = number1 - number2;
+//         console.log(`${number1} - ${number2} = ${result}`);
+//         break;
+//     case '*':
+//         result = number1 * number2;
+//         console.log(`${number1} * ${number2} = ${result}`);
+//         break;
+//     case '/':
+//         result = number1 / number2;
+//         console.log(`${number1} / ${number2} = ${result}`);
+//         break;
+//     case '**':
+//         result = number1 ** number2;
+//         console.log(`${number1} ** ${number2} = ${result}`);
+//         break;
+//     case '%':
+//         result = number1 ** number2;
+//         console.log(`${number1} % ${number2} = ${result}`);
+//         break;
+
+//     default:
+//         console.log('Invalid operator');
+//         break;
+// }
